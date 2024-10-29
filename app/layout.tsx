a1455f8b64c8879081eca51d97efc0b3ab9fd97d
Nguyễn Đình Hào <91510339+NguyenHaols@users.noreply.github.com>
2024-10-29 11:12:19 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import { inter } from "./ui/font";
-import '@/app/ui/global.css';
+import { inter } from './ui/fonts'
+import '@/app/ui/global.css'
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
